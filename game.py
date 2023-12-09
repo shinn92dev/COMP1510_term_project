46bbb5c43655476ce09b5470438b40a99d68c4d4
Anthony Shin <shinn92dev@gmail.com>
2023-12-09 10:54:07 -0800

    ADD docstring for create_character

--- game.py
+++ game.py
@@ -49,6 +49,15 @@ def create_map():
 
 
 def create_character(character_information):
+    """
+    Create character dictionary
+
+    :param character_information: a dictionary
+    :precondition: character_information should be a dictionary containing name, occupation_title as keys with values
+                   from the user
+    :postcondition: create initial character information with correct user information
+    :return: a dictionary containing character information
+    """
     character = {"name": character_information["name"], "occupation": character_information["occupation_title"],
                  "location": (0, 0), "level": 1, "current_hp": 1000, "max_hp": 1000, "xp": 10, "attack": 2}
     # TODO: Add "attack": character_information.skills into character dictionary
