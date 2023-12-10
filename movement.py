681b865f8c8548488b1e63802711b0d3e0348d48
Momona Wada <momona.9410@gmail.com>
2023-12-09 18:18:11 -0800

    ADD doc tests for move_character()

--- movement.py
+++ movement.py
@@ -47,6 +47,14 @@ def move_character(user_input, character):
     :precondition: user_input is a valid string ("1" for north, "3" for south, "4" for west, "2" for east)
     :precondition: character is a dictionary containing the character's location as a tuple (x, y)
     :postcondition: character's current location will be updated based on the user input
+    >>> character_info = {"location": (0, 0)}
+    >>> move_character("2", character_info)
+    >>> character_info["location"]
+    (1, 0)
+    >>> character_info = {"location": (2, 0)}
+    >>> move_character("3", character_info)
+    >>> character_info["location"]
+    (2, 1)
     """
     current_location = list(character["location"])
     location_after_movement = current_location
