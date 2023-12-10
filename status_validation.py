8f5488780915dbf7aef4acbdc9aecfe604fc3d62
Momona Wada <momona.9410@gmail.com>
2023-12-09 18:39:59 -0800

    ADD docstring for is_enough_level_to_proceed_to_next_map()

--- status_validation.py
+++ status_validation.py
@@ -1,7 +1,16 @@
-"""
-Add Docstring
-"""
 def is_enough_level_to_proceed_to_next_map(character, entire_board, current_map):
+    """
+    Check if character has reached a high enough level to proceed the next level on map.
+
+    :param character: a dictionary containing character's information
+    :param entire_board: a dictionary containing all maps
+    :param current_map: a dictionary containing the current map where the character is located
+    :precondition: character is a dictionary with at least the keys 'level' and 'location'
+    :precondition: entire_board is a dictionary all game maps with map levels as keys.
+    :precondition: current_map is a dictionary representing the current map with character's location.
+    :postcondition: determine if the character's current level is greater than the current map's level
+    :return: True if the character's current level is greater than the current map's level, otherwise False
+    """
     current_map_level = None
     for map_level in range(1, 6):
         if current_map[character["location"]] in entire_board[map_level].values():
