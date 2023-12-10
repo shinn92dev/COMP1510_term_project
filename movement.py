bbe8ec8d538ef104c680ccdc6c262fa35178e43b
Momona Wada <momona.9410@gmail.com>
2023-12-09 17:39:56 -0800

    ADD docstring for validate_movement()

--- movement.py
+++ movement.py
@@ -1,7 +1,17 @@
-"""
-Add Docstring
-"""
 def validate_movement(user_input, character, board):
+    """
+    Check if the character's movement based on user input is valid within the board boundaries.
+
+    :param user_input: a string
+    :param character: a dictionary containing the character's information
+    :param board: a dictionary representing the board
+    :precondition: user_input is a string representing the direction of movement, either a cardinal direction or
+                   a corresponding number '1' for north, '2' for east, '3' for south, '4' for west
+    :precondition: character is a dictionary containing the character's current location with the key 'location'
+    :precondition: board must have keys that represent valid location coordinates as tuples (x, y)
+    :postcondition: determine if the user's input is valid or not
+    :return: True if the user's input is valid, otherwise False
+    """
     current_location = list(character["location"])
     location_after_movement = current_location
     if user_input == "1" or user_input == "north":
